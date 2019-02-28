97224da3b5aa2237819cf8b409e7da9cc4f4eb42
Gus Fune <gusfune@epicawesome.co>
2019-02-28 14:59:29 -0300

    Added public folder to use Github Pages

---
+++ public/path----96620debe3632e4f0069.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{98:function(e,t){e.exports={data:{site:{siteMetadata:{title:"Gus Fune",description:"Tech Lead",url:"https://gusfune.com",keywords:"tehc lead, product manager, portfolio, personal website"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----96620debe3632e4f0069.js.map

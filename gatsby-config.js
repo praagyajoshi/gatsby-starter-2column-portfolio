c891fd21d11f47c940f0d604d4717f7f280b35b4
Gus Fune <gustavofunee@gmail.com>
2019-05-24 12:53:59 -0300

    Optimizing images bootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      },
+    },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,36 @@
-import React, { Component } from 'react'
-import cspo from "../assets/images/seal-cspo.png" 
-
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from "gatsby-image";
 import './About.scss'
 
-class About extends Component {
-  render () {
-    return (
-      <div className="about">
-        <div className="image">
-        </div>
-        <div className="bio">
-          <p>I’m an experienced product manager who can fix hard-to-find bugs, test design changes in the browser, and automate anything my team needs to get done faster.</p>
-          <p>I’ve delivered over 100 projects for clients like 3M, Coca-Cola, Behold Studios, BMW, Heineken, McDonald's and Motorola. I’ve also worked with early-stage startups on everything from product research to hiring, team-building, and even accounting.</p>
-        </div>
-        <div className="certification">
-          <img src={cspo} alt="Certified Scrum Product Owner®" width="100%" height="auto" />
-        </div>
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query CSPOImage {
+        file(relativePath: { eq: "seal-cspo.png"}) {
+          childImageSharp {
+            fixed(width: 92, height: 92) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+  render={data => (
+    <div className="about">
+      <div className="image"></div>
+      <div className="bio">
+        <p>I’m an experienced product manager who can fix hard-to-find bugs, test design changes in the browser, and automate anything my team needs to get done faster.</p>
+        <p>I’ve delivered over 100 projects for clients like 3M, Coca-Cola, Behold Studios, BMW, Heineken, McDonald's and Motorola. I’ve also worked with early-stage startups on everything from product research to hiring, team-building, and even accounting.</p>
       </div>
-    )
-  }
-}
-
-export default About
+      <div className="certification">
+      <Img
+        title="Certified Scrum Product Owner®"
+        alt="Certified Scrum Product Owner®"
+        fixed={data.file.childImageSharp.fixed}
+      />
+      </div>
+    </div>
+  )}
+  />
+)

7f8adde8f961021b4fd145cc564ff6a8c0117e1a
Gus Fune <gustavofunee@gmail.com>
2019-06-15 17:09:15 -0300

    Removed carousel from Recommendations

--- src/components/RecommendationSection.js
+++ src/components/RecommendationSection.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import Section from './Section'
 import RecommendationUnit from './RecommendationUnit'
-import Slider from "react-slick";
 import { StaticQuery, graphql } from 'gatsby'
 
-const SliderSettings = {
-  dots: true,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  arrows: false
-};
 export default ( children ) => (
   <StaticQuery
     query={graphql`
@@ -35,17 +28,15 @@ export default ( children ) => (
     `}
     render={data => (
         <Section title="This is what some people said about working with me" className="recommendationSection">
-          <Slider {...SliderSettings}>
-            {data.allContentfulRecommendation.nodes.map(node => (
-              <RecommendationUnit
-                id={node.id}
-                photo={node.photo}
-                name={node.name}
-                context={node.context}
-                text={node.content.content}
-              />
-            ))}
-          </Slider>
+          {data.allContentfulRecommendation.nodes.map(node => (
+            <RecommendationUnit
+              id={node.id}
+              photo={node.photo}
+              name={node.name}
+              context={node.context}
+              text={node.content.content}
+            />
+          ))}
         </Section>
     )}
   />
--- src/components/RecommendationUnit.js
+++ src/components/RecommendationUnit.js
@@ -6,15 +6,16 @@ class RecommendationUnit extends Component {
   render() {
     return (
       <div className="recommendation-unit row" id={this.props.id}>
-          <div className="col-xs-2">
+          <div className="col-xs-2 col-sm-1">
             <img
               className="image"
               src={this.props.photo.file.url}
               title={this.props.photo.title}
               alt={this.props.photo.title}
+              width="100%"
             />
           </div>
-          <div className="col-xs-10">
+          <div className="col-xs-10 col-sm-11">
             <h2 className="title bold">
               {this.props.name}
             </h2>

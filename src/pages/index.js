f4c14e30862fdadda5ea9e4c6a390824ce8c308b
Gus Fune <gustavofunee@gmail.com>
2019-08-11 15:00:18 +0200

    Added Cookie Consent base and Prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import ProjectsSection from '../components/ProjectsSection'
-import About from '../components/About'
-import Links from '../components/Links'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import './index.scss'
-//import get from 'lodash/get'
-import { graphql } from 'gatsby'
+import React from "react"
+import ProjectsSection from "../components/ProjectsSection"
+import About from "../components/About"
+import Links from "../components/Links"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import "./index.scss"
 
-import profileImage from '../assets/images/gusfune.jpg'
-import favicon16 from '../assets/favicons/favicon-16x16.png'
-import favicon32 from '../assets/favicons/favicon-32x32.png'
-import RecommendationSection from '../components/RecommendationSection';
+import { graphql } from "gatsby"
+import CookieConsent from "react-cookie-consent"
+import profileImage from "../assets/images/gusfune.jpg"
+import favicon16 from "../assets/favicons/favicon-16x16.png"
+import favicon32 from "../assets/favicons/favicon-32x32.png"
+import RecommendationSection from "../components/RecommendationSection"
 
 const IndexPage = ({ data }) => (
   <div className="template-wrapper">
@@ -19,18 +19,33 @@ const IndexPage = ({ data }) => (
       <Helmet
         title={data.site.siteMetadata.title}
         meta={[
-          { name: 'description', content: data.site.siteMetadata.description },
-          { name: 'keywords', content: data.site.siteMetadata.keywords },
-          { property: 'og:url', content: data.site.siteMetadata.url },
-          { property: 'og:image', content: profileImage },
-          { property: 'og:title', content: data.site.siteMetadata.title },
-          { property: 'og:description', content: data.site.siteMetadata.description }
+          { name: "description", content: data.site.siteMetadata.description },
+          { name: "keywords", content: data.site.siteMetadata.keywords },
+          { property: "og:url", content: data.site.siteMetadata.url },
+          { property: "og:image", content: profileImage },
+          { property: "og:title", content: data.site.siteMetadata.title },
+          {
+            property: "og:description",
+            content: data.site.siteMetadata.description,
+          },
         ]}
         link={[
-          { rel: 'icon', type: 'image/png', sizes: '16x16', href: favicon16 },
-          { rel: 'icon', type: 'image/png', sizes: '32x32', href: favicon32 },
-          { rel: 'stylesheet', type: 'text/css', charset: 'UTF-8', href: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" },
-          { rel: 'stylesheet', type: 'text/css', sizes: '32x32', href: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" }
+          { rel: "icon", type: "image/png", sizes: "16x16", href: favicon16 },
+          { rel: "icon", type: "image/png", sizes: "32x32", href: favicon32 },
+          {
+            rel: "stylesheet",
+            type: "text/css",
+            charset: "UTF-8",
+            href:
+              "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css",
+          },
+          {
+            rel: "stylesheet",
+            type: "text/css",
+            sizes: "32x32",
+            href:
+              "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css",
+          },
         ]}
       />
       <div className="main">
@@ -39,7 +54,8 @@ const IndexPage = ({ data }) => (
         </h2>
 
         <h3 className="bold">
-          I'm a highly technical Web & Mobile Product Manager who delivered over 100 products to scale.
+          I'm a highly technical Web & Mobile Product Manager who delivered over
+          100 products to scale.
         </h3>
         <ProjectsSection />
         <RecommendationSection />

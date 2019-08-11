04cc9adc7d24c39b9be35fcb947d0fde73e045a7
Gus Fune <gustavofunee@gmail.com>
2019-08-11 15:37:55 +0200

    Fix tagmanager missing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,23 @@
-require('dotenv').config()
+require("dotenv").config()
 
 module.exports = {
   plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`
+        path: `${__dirname}/src/assets/images`,
       },
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `open sans\:300,600`
-        ]
-      }
+        fonts: [`open sans\:300,600`],
+      },
     },
     {
       resolve: `gatsby-source-contentful`,
@@ -29,18 +27,12 @@ module.exports = {
         downloadLocal: true,
       },
     },
-    {
-      resolve: `gatsby-plugin-google-tagmanager`,
-      options: {
-        id: process.env.GOOGLE_TAGMANAGER_ID,
-        includeInDevelopment: true,
-      }
-    }
   ],
   siteMetadata: {
-    title: 'Gus Fune',
-    description: 'I\'m a highly technical Web & Mobile Product Manager who delivered over 100 products to scale.',
-    keywords: 'tech lead, product manager, portfolio, personal website',
-    url: 'https://gusfune.com'
-  }
-};
+    title: "Gus Fune",
+    description:
+      "I'm a highly technical Web & Mobile Product Manager who delivered over 100 products to scale.",
+    keywords: "tech lead, product manager, portfolio, personal website",
+    url: "https://gusfune.com",
+  },
+}

c7e7f6e5ab598718cb9390098bc5c14b52dcc1ca
Gus Fune <gustavofunee@gmail.com>
2019-08-17 15:07:01 +0200

    Language Detector in place

--- src/components/i18n.js
+++ src/components/i18n.js
@@ -1,4 +1,5 @@
 import i18n from "i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next"
 
 const resources = {
@@ -19,13 +20,16 @@ const resources = {
   },
 }
 
-i18n.use(initReactI18next).init({
-  resources,
-  fallbackLng: "en",
-  keySeparator: false,
-  interpolation: {
-    escapeValue: false,
-  },
-})
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en",
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  })
 
 export default i18n

58437059fc94348d8b722dcbdd417406bf6701f1
Gus Fune <gustavofunee@gmail.com>
2019-08-17 15:03:43 +0200

    Update i18n.js

--- src/components/i18n.js
+++ src/components/i18n.js
@@ -1,7 +1,6 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
-// the translations
 const resources = {
   en: {
     translation: {
@@ -13,22 +12,20 @@ const resources = {
       hi: "Oi",
     },
   },
-  se: {
+  sv: {
     translation: {
       hi: "Hej",
     },
   },
 }
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-    fallbackLng: "en",
-    keySeparator: false, // we do not use keys in form messages.welcome
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
-  })
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: "en",
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+})
 
 export default i18n

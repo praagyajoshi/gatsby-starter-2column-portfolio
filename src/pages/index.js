832e0c255a65fbaf7cabb31b269eaa6c228fe0cb
Gus Fune <gustavofunee@gmail.com>
2019-08-17 16:10:26 +0200

    Migrated Helmet to Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Helmet from "react-helmet"
 import PropTypes from "prop-types"
 import { Trans } from "react-i18next"
 
 import Layout from "../components/layout"
 import "../components/i18n"
-import profileImage from "../assets/images/gusfune.jpg"
-import favicon16 from "../assets/favicons/favicon-16x16.png"
-import favicon32 from "../assets/favicons/favicon-32x32.png"
 import ProjectsSection from "../components/ProjectsSection"
 import RecommendationSection from "../components/RecommendationSection"
 
 const IndexPage = ({ data }) => (
   <div className="template-wrapper">
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: "description", content: data.site.siteMetadata.description },
-        { name: "keywords", content: data.site.siteMetadata.keywords },
-        { property: "og:url", content: data.site.siteMetadata.url },
-        { property: "og:image", content: profileImage },
-        { property: "og:title", content: data.site.siteMetadata.title },
-        {
-          property: "og:description",
-          content: data.site.siteMetadata.description,
-        },
-      ]}
-      link={[
-        { rel: "icon", type: "image/png", sizes: "16x16", href: favicon16 },
-        { rel: "icon", type: "image/png", sizes: "32x32", href: favicon32 },
-        {
-          rel: "stylesheet",
-          type: "text/css",
-          charset: "UTF-8",
-          href:
-            "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css",
-        },
-        {
-          rel: "stylesheet",
-          type: "text/css",
-          sizes: "32x32",
-          href:
-            "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css",
-        },
-      ]}
-    />
     <Layout>
       <h2>
         <Trans i18nKey="hi">Hi</Trans>, I'm{" "}
